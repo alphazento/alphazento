2fed294b26dbfa80664efba3a253d523ea6db17f
chen <chen@chens-MacBook-Pro.local>
2020-04-05 16:08:55 +1000

    init

---
+++ config/zento/Zento_Kernel.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'dynamicattribute_management' => true,
+    'config_extend' => [
+        'extra_repository' => \Zento\Kernel\Booster\Config\ConfigInDB\ConfigRepository::class,
+        'grouping_provider' => null         //config can be different by grouping
+    ],
+    'mypackages_folder' => 'packages'
+];
